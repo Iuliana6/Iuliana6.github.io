9dda31c98af5d7d57951d151088d6ef825285605
Iuliana6 <iulianacapilnar@yahoo.com>
2021-11-11 17:43:49 +0200

    Added sorting select boxes

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-
 var vue = new Vue({
     el: '#container',
     data: {
         lessons: lessons,
-        cart:[],
-        lessonListShown:true,
-        cartShown:false,
-        checkoutButtonShown:false,
+        cart: [],
+        lessonListShown: true,
+        cartShown: false,
+        checkoutButtonShown: false,
+        sortAscending: true,
+        sortingCriteria:["Subject","Location","Price","Availability"],
+        sortingOrder:["Ascending","Descending"]
+    },
+    computed: {
+        sortedLessons() {
+            let compare=(a, b)=> {
+                //if ascending is false flip the order
+                if (a.price > b.price) {
+                    return this.sortAscending ? 1 : -1;
+                }
+                if (a.price < b.price) return this.sortAscending ? -1 : 1;
+                return 0;
+            }
+
+            return this.lessons.sort(compare);
+        }
     },
     methods: {
         addToCart: function f(lesson) {
@@ -25,11 +41,11 @@ var vue = new Vue({
             if (lessonIndex === -1) {
                 //add lesson to the cart
                 this.cart.push({
-                    subject:lesson.subject,
-                    location:lesson.location,
-                    price:lesson.price,
-                    quantity:1,
-                    imageURL:lesson.imageURL
+                    subject: lesson.subject,
+                    location: lesson.location,
+                    price: lesson.price,
+                    quantity: 1,
+                    imageURL: lesson.imageURL
                 })
             } else {
                 //if already found increase quantity
@@ -39,36 +55,36 @@ var vue = new Vue({
             lesson.space = lesson.space - 1
 
             //show the checkout button
-            this.checkoutButtonShown=true
+            this.checkoutButtonShown = true
         },
-        removeFromCart:function f(lesson){
-            this.lessons.forEach(l=>{
+        removeFromCart: function f(lesson) {
+            this.lessons.forEach(l => {
                 //add quantity back to lesson list
-                if(l.subject===lesson.subject){
-                    l.space+=lesson.quantity
+                if (l.subject === lesson.subject) {
+                    l.space += lesson.quantity
                 }
             })
             //remove lesson from cart
-            this.cart=this.cart.filter(l=>l.subject!==lesson.subject)
+            this.cart = this.cart.filter(l => l.subject !== lesson.subject)
         },
-        toggleCart:function f(){
-            this.cartShown=!this.cartShown;
-            this.lessonListShown=!this.lessonListShown;
+        toggleCart: function f() {
+            this.cartShown = !this.cartShown;
+            this.lessonListShown = !this.lessonListShown;
         },
 
-        checkout:function f(){
-            let name=document.getElementById("name-input").value
-            let phoneNumber=document.getElementById("phone-input").value
-            for(let i=0;i<name.length;i++){
-                let c=name.charAt(i)
-                if(!((c>='a'&&c<='z')||(c>='A'&&c<='Z')||c===' ')){
+        checkout: function f() {
+            let name = document.getElementById("name-input").value
+            let phoneNumber = document.getElementById("phone-input").value
+            for (let i = 0; i < name.length; i++) {
+                let c = name.charAt(i)
+                if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c === ' ')) {
                     alert("Invalid name")
                     return;
                 }
             }
-            for(let i=0;i<phoneNumber.length;i++){
-                let c=phoneNumber.charAt(i)
-                if(!(c>='0'&&c<='9')){
+            for (let i = 0; i < phoneNumber.length; i++) {
+                let c = phoneNumber.charAt(i)
+                if (!(c >= '0' && c <= '9')) {
                     alert("Invalid phone number")
                     return
                 }
--- lessons.js
+++ lessons.js
@@ -2,21 +2,21 @@ let lessons=[
     {
         subject:"Math",
         location:"Location",
-        price:100,
+        price:80,
         space:10,
         imageURL:"images/math.png"
     },
     {
         subject:"Subject",
         location:"Location",
-        price:100,
+        price:67,
         space:10,
         imageURL:"images/image0"
     },
     {
         subject:"Subject",
         location:"Location",
-        price:100,
+        price:75,
         space:10,
         imageURL:"images/image0"
     },
